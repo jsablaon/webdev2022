4b49d85a6caf9f75ef7212d47c1b59710f6ea402
John <82200615+jsablaon@users.noreply.github.com>
2022-03-29 11:24:13 -0700

    added out-view (#2)

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-(function () {
-    "use strict";
-  
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    "use strict"; // cleaner code
+
     // define variables
-    var items = document.querySelectorAll(".timeline li");
-  
+    // grabs verticalLine class and all li elem into a collection
+    var items = document.querySelectorAll(".verticalLine li");
+
     // check if an element is in viewport
     // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
     function isElementInViewport(el) {
-      var rect = el.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-          (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
+        var rect = el.getBoundingClientRect();
+
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
     }
-  
-    function callbackFunc() {
-      for (var i = 0; i < items.length; i++) {
-        if (isElementInViewport(items[i])) {
-          items[i].classList.add("in-view");
+
+    // mark li elememts in-view/out-view
+    function markElements() {
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList = "in-view";
+            } else {
+                items[i].classList = "out-view";
+            }
         }
-      }
     }
-  
-    // listen for events
-    window.addEventListener("load", callbackFunc);
-    window.addEventListener("resize", callbackFunc);
-    window.addEventListener("scroll", callbackFunc);
-  })();
-  
+
+    // listen for events in current open window
+    window.addEventListener("load", markElements);
+    window.addEventListener("resize", markElements);
+    window.addEventListener("scroll", markElements);
+})
