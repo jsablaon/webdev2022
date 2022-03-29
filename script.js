f9a1f03f13d3a279df59b706e73d7e1224277cd8
John <82200615+jsablaon@users.noreply.github.com>
2022-03-29 14:32:17 -0700

    added year to footer (#7)

--- script.js
+++ script.js
@@ -32,6 +32,18 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    let currentYear = new Date().getFullYear();
+    console.log(currentYear);
+
+    let footer = document.getElementsByClassName('page-footer');
+    // console.log(footer, "footer");
+
+    let newSpan = document.createElement("span");
+    newSpan.setAttribute("id", "span-footer");
+    newSpan.innerHTML = `&copy; jsablaon ${currentYear}`;
+    // since footer is a collection of DOM nodes, need to specify which node to append the span to.
+    footer[0].appendChild(newSpan);
+
     // listen for events in current open window
     window.addEventListener("load", markElements);
     window.addEventListener("resize", markElements);
